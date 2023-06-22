308f76bd21e1436e5bbe5154973889a62370a29f
Karaniare <74371074+Karaniare@users.noreply.github.com>
2023-06-22 15:59:21 -0400

    Update Making_VCFs.sh

--- Making_VCFs.sh
+++ Making_VCFs.sh
@@ -6,12 +6,9 @@
 #
 ###### Creating arguments
 usage() {
-  "Usage:$0 or bash /opt/resources/Making_VCFs.sh [-i <input>] [-b <bam directory>] [-v <VCF directory>] [-g <gVCF directory>] [-c <chromosome number>] [-t <region>]" 1>&2
-  echo "-i: input file (sample ID list) in tab separated value format"
+  "Usage:$0 or bash /opt/resources/Making_VCFs.sh [-v <VCF directory>] [-c <chromosome number>] [-t <region>]" 1>&2
   echo "-c: chromosome number (ex.: 1)"
-  echo "-b: bam file directory"
   echo "-v: directory to save VCF files"
-  echo "-g: gVCF file directory"
   echo "-t: file containing a list of genomic regions to call variants at, one region per line (ex.: 1-20,000  20,001-40,000 40,001-60,000 60,001-80,000)"
   exit 1
 }
@@ -20,24 +17,15 @@ usage() {
 while getopts i:b:v:g:c:t: flag
 do
     case "${flag}" in
-        i) input=${OPTARG};;
-        b) bam_dir=${OPTARG};;
         v) vcf_dir=${OPTARG};;
-        g) gvcf_dir=${OPTARG};;
-	c) chrom=${OPTARG};;
-	t) region=${OPTARG};;
+        c) chrom=${OPTARG};;
+        t) region=${OPTARG};;
     esac
 done
-echo "##################This package works on a server with slurm job submissions#################"
-echo "Argument definition:"
-echo "-i: input file (sample ID list) in tab separated format"
-echo "-t: genomic region file"
-echo "-b: bam file directory"
-echo "-v : vcf file (or output) directory"
-echo "-g: gvcf file directory"
-echo "-t: specify a file containing a list of genomic regions to call variants such as 1-20,000  20,001-40,000 40,001-60,000 60,001-80,000"
+
 ###
-ref_dir="/opt/data"
+#ref_dir="/opt/data"
+ref_dir="/users/kniare/data/shared/WGS_works_Karamoko/scripts/Data_WGS_UNC"
 
 ######## Combining gVCFs into a genomic database 
 ###### Combining gvcfs 
@@ -64,7 +52,7 @@ for i in $chrom
      echo -e "#SBATCH -t 120:00:00" >> genotype_chr"$i"_"$j".sh
      echo -e "#SBATCH -c 8" >> genotype_chr"$i"_"$j".sh
      echo -e "#SBATCH --mem-per-cpu=10g" >> genotype_chr"$i"_"$j".sh
-     echo -e "module load gatk/4.1.6.0 samtools/ bcftools/1.9 bcftools/1.9" >> genotype_chr"$i"_"$j".sh
+     echo -e "module load gatk/4.2.2.0 samtools/ bcftools/1.9 bcftools/1.9" >> genotype_chr"$i"_"$j".sh
      echo -e "ulimit -c unlimited" >> genotype_chr"$i"_"$j".sh
      echo -e "module load java/jdk-17.0.2" >> genotype_chr"$i"_"$j".sh
      echo -e "gatk --java-options "-Xmx80g -Xms80g"  GenotypeGVCFs --genomicsdb-use-bcf-codec true  -R $ref_dir/Pf3D7.fasta -V gendb://$vcf_dir/chr"$i"_database --max-genotype-count 1024 -O $vcf_dir/chr"$i"_part"$j".vcf.gz --tmp-dir $ref_dir -stand-call-conf 30 -L "$j"" >> genotype_chr"$i"_"$j".sh
@@ -73,4 +61,3 @@ for i in $chrom
      sbatch genotype_chr"$i"_"$j".sh
    done
 done
-
